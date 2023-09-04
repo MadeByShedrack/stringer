83b79105ebd29abdd79db4cf9cf36782bc099283
MadeByShedrack <madebyshedrack@gmail.com>
2023-09-04 12:28:42 +0100

    third commit

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 
-	for range time.Tick(time.Second * 2) {
-		fmt.Printf("The time is -> %v\n", time.Now())
+	// for range time.Tick(time.Second * 2) {
+	// 	fmt.Printf("The time is -> %v\n", time.Now())
+	// }
+
+	userName := "john"
+
+	for _, u := range userName {
+		fmt.Printf("Name letters -> %v\n", u)
 	}
 }
